80e87f64b9bb97febcdbd950c51670f2ce60dfae
John Galt <johngaltfirstrun@gmail.com>
2022-05-06 14:34:16 -0400

    sm8350-common: Enable FUSE Passthrough
    [rk134]: we have k5.4, which has FUSE Passthrough support by default.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: rk134 <rahul.kantrapally@gmail.com>

--- common.mk
+++ common.mk
@@ -145,6 +145,10 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
 
+# FUSE passthrough
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.fuse.passthrough.enable=true
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
