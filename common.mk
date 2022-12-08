f6edd24f2a7b496358f9de090cfbe35f022185da
Jake Weinstein <jake@aospa.co>
2022-12-09 00:35:52 +0900

    sm8350-common: Do not balance msm_drm and kgsl_3d0 IRQs
    These are critical for display performance and should not be balanced
    in order to improve latency and responsiveness.
    
    And also affine them.
    
    Change-Id: If49ecb8757d133a7fad0d7946837b35403e57c2a
    Signed-off-by: rk134 <rahul.kantrapally@gmail.com>

--- common.mk
+++ common.mk
@@ -246,6 +246,10 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_enable)
 
+# Performance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor # Needed by CNE app
